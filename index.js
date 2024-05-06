48fd59982f1baa49499c400a3365d3b541d05303
Luciano <lbinder@lasalleflorida.edu.ar>
2024-05-06 13:24:56 -0300

    Trabajo api

--- index.js
+++ index.js
@@ -1,34 +1,63 @@
-const express = require('express');
-const cors = require('cors');
-const path = require('path'); // Import path module
-const app = express();
-const port = 43922;
-// Allow all cross-origin requests
-app.use(cors());
-// Middleware to parse JSON bodies
+// Importamos las dependencias para poder usarlas dentro de ...
+//nuestro codigo
+ const express = require('express');
+ const cors = require('cors');
+ const path = require('path');
+
+ // Inicializamos la aplicacion y elegimos el puerto en el que ...
+//nuestro servidor escuchara aplicaciones.
+ const app = express();
+ const port = 43922;
+
+ // Habilitamos el middleware CORS en la aplicaci n
+ app.use(cors());
+
+ // Este middleware es responsable de analizar el cuerpo de las ...
+//solicitudes entrantes con el tipo de contenido application/json.
 app.use(express.json());
+
 app.get('/', (req, res) => {
-    console.log(__dirname)
-
-    try {
-
-        res.sendFile(path.join(__dirname, 'index.html'));
-    } catch(e) {
-        res.status(500).send({'error': 'Internal server error'})
+         try{
+         // Aca va lo que quiero que funcione.
+         res.sendFile(path.join( dirname, 'index.html'));
+         } catch(e){
+         // Manejo de errores si falla lo anterior.
+         res.status(500).send({'error': 'Internal server error'});
+        
     }
-    
-
+    9 
 });
-/*
-------------------
-Aca van los endpoints.
-------------------
-*/
 
-// Nuestra aplicación esta lista para recibir peticiones
-app.listen(port, () => {
+    
+    // Creamos dos arreglos en donde almacenaremos los mails y los ...
+    //usuarios.
+     const arreglo_usuarios = Array()
+     const arreglo_mails = Array()
+    
+     app.post('/', (req, res) => {
+    
+    // Guardamos el usuario y el email que venian en el Json del ...
+    
+    //body en dos variables.
+     const {usuario, email} = req.body;
+    
+     // Guardamos el usuario en el arreglo de usuarios.
+     arreglo_usuarios.push(usuario)
+     arreglo_mails.push(email)
+    
+     // Imprimimos los arrays para ver si se estan agregando los ...
+       // elementos
+    console.log(arreglo_usuarios)
+     console.log(arreglo_mails)
+    
+     // Devolvemos un codigo de respuesta 201 indicando que el ...
+    /*recurso fue creado con exito junto con el usuario y el ...
+    mail que fueron recibidos.*/
+    res.status(201).send({usuario,email})
+     });
 
-    console.log('hola ${port}');
-
-                        }
-);
+ // Este m t o d o inicia el servidor Express y lo pone a la ...
+//escucha de solicitudes entrantes en un puerto espec fico.
+ app.listen(port, () => {
+console.log(`Server is running on http://localhost:${port}`);
+ });
